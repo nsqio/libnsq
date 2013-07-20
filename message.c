6caad69b583128397eb20e53bf7e96db9d4c6129
Matt Reiferson <mreiferson@gmail.com>
2013-07-20 11:34:44 -0400

    simpler universal ntoh64

--- message.c
+++ message.c
@@ -6,21 +6,11 @@
 #define _DEBUG(...) do {;} while (0)
 #endif
 
-uint64_t ntoh64(const uint64_t *input)
-{
-    uint64_t rval;
-    uint8_t *data = (uint8_t *)&rval;
-    
-    data[0] = *input >> 56;
-    data[1] = *input >> 48;
-    data[2] = *input >> 40;
-    data[3] = *input >> 32;
-    data[4] = *input >> 24;
-    data[5] = *input >> 16;
-    data[6] = *input >> 8;
-    data[7] = *input >> 0;
-    
-    return rval;
+uint64_t ntoh64(const uint8_t *data) {
+    return (uint64_t)(data[7]) | (uint64_t)(data[6])<<8 | 
+        (uint64_t)(data[5])<<16 | (uint64_t)(data[4])<<24 |
+        (uint64_t)(data[3])<<32 | (uint64_t)(data[2])<<40 | 
+        (uint64_t)(data[1])<<48 | (uint64_t)(data[0])<<56;
 }
 
 struct NSQMessage *nsq_decode_message(const char *data, size_t data_length)
@@ -29,7 +19,7 @@ struct NSQMessage *nsq_decode_message(const char *data, size_t data_length)
     size_t body_length;
     
     msg = malloc(sizeof(struct NSQMessage));
-    msg->timestamp = (int64_t)ntoh64((uint64_t *)data);
+    msg->timestamp = (int64_t)ntoh64((uint8_t *)data);
     msg->attempts = ntohs(*(uint16_t *)(data+8));
     memcpy(&msg->id, data+10, 16);
     body_length = data_length - 26;
